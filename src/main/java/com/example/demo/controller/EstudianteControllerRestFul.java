ce8ad0b1a9d19bf8b31a796a0e28dc23ed0729be
wadiaz98 <wadiaz@uce.edu.ec>
2024-01-27 10:50:56 -0500

    taller27

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.modelo.Estudiante;
+import com.example.demo.service.IEstudianteService;
 
 //Una API puede tener muchos servicios
 //API: determinada por el proyecto Java
@@ -11,12 +16,22 @@ import com.example.demo.repository.modelo.Estudiante;
 //Servicio -> Controller: Clase controller
 
 @RestController // Servicio
-
+@RequestMapping(path = "/estudiantes")
 public class EstudianteControllerRestFul {
 
-	// Métodos: Capacidades
-	public void guardar(Estudiante estudiante) {
+	@Autowired
+	private IEstudianteService estudianteService;
 
+	// Métodos: Capacidades
+	// GET
+	@GetMapping(path = "/buscar")
+	public Estudiante buscar() {
+		return this.estudianteService.buscar(1);
 	}
 
+	@PostMapping(path = "/guardar")
+	public void guardar(Estudiante estudiante) {
+		this.estudianteService.guardar(estudiante);
+	}
+	// http://localhost:8080/API/v1.0/Matricula/estudiantes/buscar
 }
